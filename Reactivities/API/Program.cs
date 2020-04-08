7e485df9318971b809d163d9d3255461a1671862
iadaria <jadarya@mail.ru>
2020-04-08 13:27:13 +0900

    feat: add the List with Handler

--- Reactivities/API/Program.cs
+++ Reactivities/API/Program.cs
@@ -22,10 +22,14 @@ namespace API
             using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
-                try {
+                try 
+                {
                     var context = services.GetRequiredService<DataContext>();
                     context.Database.Migrate(); //if not exist then create
-                } catch (Exception ex) {
+                    //Seed.SeedData(context);
+                } 
+                catch (Exception ex) 
+                {
                     var logger = services.GetRequiredService<ILogger<Program>>();
                     logger.LogError(ex, "An error occured during migration");
                 }

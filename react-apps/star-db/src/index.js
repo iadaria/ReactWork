2894e417aae04636514c72ac2303af5cd12c0625
Daria Iakimova <dahunichka@gmail.com>
2020-04-15 15:33:27 +0900

    _

--- react-apps/star-db/src/index.js
+++ react-apps/star-db/src/index.js
@@ -1,7 +1,15 @@
-
+import SwapiService from './services/swapi-service.js';
 
 const swapi = new SwapiService();
 
 swapi.getAllPeople().then((body) => {
+    console.log(body);
+});
+
+swapi.getAllPlanets().then((body) => {
+    console.log(body);
+});
+
+swapi.getAllStarships().then((body) => {
     console.log(body);
 });

56de1b2d2aa4f86e2c515d33012ab6aa27b6720d
iadaria <jadarya@mail.ru>
2020-06-10 07:41:06 +0900

    research doc of redux

---
+++ redux/doc1/src/reducers/index.js
@@ -0,0 +1,78 @@
+import { combineReducers } from 'redux';
+import {
+  ADD_TODO,
+  TOGGLE_TODO,
+  SET_VISIBILITY_FILTER,
+  VisibilityFilters,
+} from "../actions";
+
+/* const initialState = {
+  visibilityFilter: VisibilityFilters.SHOW_ALL,
+  todos: [],
+}; */
+
+const { SHOW_ALL } = VisibilityFilters;
+
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          text: action.text,
+          completed: false,
+        },
+      ];
+
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+        if (index === action.index) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo; //without changes
+      });
+
+    default:
+      return state;
+  }
+}
+
+function visibilityFilter(state = SHOW_ALL, action) {
+  switch(action.type) {
+    case SET_VISIBILITY_FILTER:
+      return action.filter;
+    default:
+      return state;
+  }
+}
+
+const todoApp = combineReducers({
+  visibilityFilter,
+  todos
+});
+
+export default todoApp;
+
+/* function todoApp(state = {}, action) {
+  return {
+    visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+    todos: todos(state.todos, action)
+  };
+} */
+
+/* {
+  visibilityFilter: 'SHOW_ALL',
+  todos: [
+    {
+      text: 'Consider using Redux',
+      completed: true
+    },
+    {
+      text: 'Keep all state in a single tree',
+      completed: false
+    }
+  ]
+} */

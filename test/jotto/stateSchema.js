7a012ad2b769c0089f0bbe0225680233f07f7c65
iadaria <jadarya@mail.ru>
2020-06-10 06:28:04 +0900

    add test async

---
+++ test/jotto/stateSchema.js
@@ -0,0 +1,9 @@
+const stateSchema = 
+{
+  secretWord: 'party',
+  success: false,
+  guessedWords: [ 
+    { guessedWord: 'train', letterMatchCount: 3 },
+    { guessedWord: 'par', letterMatchCount: 3 },
+  ]
+};

436d3d614a54a6e4cb4b172bb3ed902e51413986
iadaria <jadarya@mail.ru>
2020-06-11 08:48:29 +0900

    test

--- test/jotto-hook-context/src/App.js
+++ test/jotto-hook-context/src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import hookActions from './actions/hookActions';
+import Input from './Input';
+/**
+ * 
+ * @param {object} state - existing state
+ * @param {object} action  - containes 'type' and 'payload', for example:{ type: "setSecretWord", payload: "party"}
+ * 
+ * @returns {object} -new state
+ */
+function reducer(state, action) {
+  switch(action.type) {
+    case "setSecretWord":
+      return {...state, secretWord: action.payload}
+    
+    default:
+      throw new Error(`Invalid action type: ${action.type}`);
+  }
+}
 
 function App() {
+  const [state, dispatch] = React.useReducer(
+    reducer,
+    { secretWord: "" }
+  );
+
+  const setSecretWord = (secretWord) => 
+    dispatch({ type: "setSecretWord", payload: secretWord});
+
+  React.useEffect(
+    () => { hookActions.getSecretWord(setSecretWord)},
+    []
+  );
+
+  if (!state.secretWord) return <Spinner />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" data-test="component-app">
+      <Input secretWord={state.secretWord}/>
     </div>
   );
+}
+
+function Spinner() {
+  return (
+    <div className="container" data-test="spinner">
+      <div className="spinner-border" role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
+      <p>Loading secret word</p>
+    </div>
+  )
 }
 
 export default App;
--- test/jotto-hook-context/src/App.test.js
+++ test/jotto-hook-context/src/App.test.js
@@ -1,7 +1,90 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { mount } from 'enzyme';
+import { findByTestAttr } from '../test/testUtils';
 import App from './App';
 
-test('renders learn react link', () => {
-  
+import hookAtions from './actions/hookActions';
+
+const mockGetSecretWord  = jest.fn();
+
+/**
+ * Setup function for app component.
+ * //@returns {ShallowWrapper}
+ */
+const setup = (secretWord = "party") => {
+  mockGetSecretWord.mockClear(); // it is necessary
+  hookAtions.getSecretWord = mockGetSecretWord;
+
+  const mockUseReducer = jest.fn()
+    .mockReturnValue([
+      { secretWord },
+      jest.fn()
+    ]);
+
+  React.useReducer = mockUseReducer;
+
+  // use mount, because useEffect not called on 'shallow'
+  return mount(<App />); // for use effect it needs to use 'mount'
+};
+
+test('App renders without error', () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, 'component-app');
+  expect(component.length).toBe(1);
+});
+
+describe('getSecretWord calls', () => {
+  test('getSecretWord gets called on App mount', () => {
+    setup();
+
+    // check to see if secret word was updated
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
+
+  test('secretWord does not update on App update', () => {
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
+    // wrpapper.update() doesn't trigger update
+    //(issue forked from https://github.com/airbnb/enzyme/issues/2091)
+    wrapper.setProps();
+
+    expect(mockGetSecretWord).not.toHaveBeenCalled();
+  });
+
+});
+
+describe('secretWord is not null', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup("party");
+  });
+
+  test("does not renders app when secretWord is not null", () => {
+    const appComponent = findByTestAttr(wrapper, "component-app");
+    expect(appComponent.exists()).toBe(true);
+  });
+
+  test("does not render spinner when secretWord is not null", () => {
+    const spinnerComponent = findByTestAttr(wrapper, "spinner");
+    expect(spinnerComponent.exists()).toBe(false);
+  });
+
+});
+
+describe('secretWord is null', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup(null);
+  });
+
+  test("does not renders app when secretWord is null", () => {
+    const appComponent = findByTestAttr(wrapper, "component-app");
+    expect(appComponent.exists()).toBe(false);
+  });
+
+  test("does not render spinner when secretWord is null", () => {
+    const spinnerComponent = findByTestAttr(wrapper, "spinner");
+    expect(spinnerComponent.exists()).toBe(true);
+  });
+
 });

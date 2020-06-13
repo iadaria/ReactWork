077568ae8e44af3545fd4ecf3182ba4ad63c9615
iadaria <jadarya@mail.ru>
2020-06-13 15:43:32 +0900

    update

--- test/jest-hook-context-props/jotto/stateSchema.js
+++
@@ -1,9 +0,0 @@
-const stateSchema = 
-{
-  secretWord: 'party',
-  success: false,
-  guessedWords: [ 
-    { guessedWord: 'train', letterMatchCount: 3 },
-    { guessedWord: 'par', letterMatchCount: 3 },
-  ]
-};

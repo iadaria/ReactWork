ae91620c823915c5771d967189827a4f80dff64d
iadaria <jadarya@mail.ru>
2020-07-03 08:33:18 +0900

    init

---
+++ revents/src/app/layout/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dasha h</h1>
+    </div>
+  );
+}
+
+export default App;

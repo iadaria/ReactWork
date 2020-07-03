dd2abb589ccc6553e13faab016e87aa76ca94f94
iadaria <jadarya@mail.ru>
2020-07-04 04:49:30 +0900

    added the file structure

---
+++ revents/src/features/user/Settings/SettingsDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SettingsDashboard = () => {
+    return (
+        <div>
+            SettingsDashboard
+        </div>
+    )
+}
+
+export default SettingsDashboard
